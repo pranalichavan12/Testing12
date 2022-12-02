164be296fe2dee5effd441caeb46cd437fa98bb7
Admin <Admin@SHEKHAR-SHENDE2021>
2022-12-02 14:20:10 +0530

    Bhagyashree: This is Ref

---
+++ src/keyword/Demo.java
@@ -0,0 +1,22 @@
+package keyword;
+
+public class Demo 
+{
+	public static void main(String[] args) 
+	{
+		int a=10;
+		Demo Demo7=new Demo();
+		Demo7.m1();
+		{
+			System.out.println(".........");
+		}
+		
+		Demo1.n2();
+	}
+	
+	public void m1()
+	{
+	System.out.println("Run as Admin");
+
+}
+}
---
+++ src/keyword/Demo1.java
@@ -0,0 +1,16 @@
+package keyword;
+
+public class Demo1
+{
+public static void main(String[] args)
+{
+	
+n2();
+
+}
+public static void n2()
+{
+System.out.println("He is Very Talented Boy");
+}
+
+}
